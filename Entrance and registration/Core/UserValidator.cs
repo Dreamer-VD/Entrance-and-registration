e58632560e74e5a1100694243f2258bad1edd2bf
Dreamer-VD <danilavacun@gmail.com>
2022-09-21 23:18:18 +0300

    first commit

---
+++ Entrance and registration/Core/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Entrance_and_registration.Core
+{
+    internal static class UserValidator
+    {
+        public static bool Validate (string login, string password)
+        {
+            if (login == "Dream" && password == "Team")
+            {
+                MessageBox.Show("Вы успешно вошли");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Неправельный пароль или логин!");
+                return false;
+            }
+        }
+    }
+}
